61e432b5696d9e3d79efdbff36c1b7219b122948
Rumeysa Kayam <rumeysakayamm@gmail.com>
2024-07-10 21:14:00 +0300

    Arithmetic Operators task is completed.

---
+++ Easy/ArithmeticOperators.py
@@ -0,0 +1,9 @@
+# Task: Arithmetic Operators 
+# HackerRank link: https://www.hackerrank.com/challenges/python-arithmetic-operators/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    a = int(input())
+    b = int(input())
+    print(a + b)
+    print(a - b)
+    print(a * b)
