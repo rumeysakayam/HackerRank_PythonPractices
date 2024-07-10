ca164f395ed8f9a861aebbdb9e0a652d6df455b8
Rumeysa Kayam <rumeysakayamm@gmail.com>
2024-07-10 21:21:45 +0300

    Loops practice completed

---
+++ Easy/Loops.py
@@ -0,0 +1,12 @@
+# Task: Loops
+# HackerRank link: https://www.hackerrank.com/challenges/python-loops/problem?isFullScreen=true
+
+if __name__ == '__main__':
+    n = int(input())
+    num = 0
+    while n > num:
+        print(num ** 2)
+        num = num + 1
+
+        
+    
