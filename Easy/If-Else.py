113a51b021e675e7b4937c82f71e54b2dc5204fe
Rumeysa Kayam <rumeysakayamm@gmail.com>
2024-07-10 21:11:58 +0300

    If-Else practice completed.

---
+++ Easy/If-Else.py
@@ -0,0 +1,23 @@
+# Task: Python If-Else
+# HackerRank link: https://www.hackerrank.com/challenges/py-if-else/problem?isFullScreen=true
+
+
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+
+if n % 2 == 1:
+    print("Weird")
+elif n % 2 == 0 and 2 <= n <= 5:
+    print("Not Weird")
+elif n % 2 == 0 and 6 <= n <= 20:
+    print("Weird")
+elif n % 2 == 0 and 20 <= n:
+    print("Not Weird")
