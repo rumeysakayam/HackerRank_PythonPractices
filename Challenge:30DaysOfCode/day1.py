56e47b36d24fc1bad30d187751cb470e6b3b6f9a
Rumeysa Kayam <rumeysakayamm@gmail.com>
2024-07-10 21:37:21 +0300

    Day 1 -  done

---
+++ Challenge:30DaysOfCode/day1.py
@@ -0,0 +1,22 @@
+# Day 1: Data Types
+
+i = 4
+d = 4.0
+s = 'HackerRank '
+# Declare second integer, double, and String variables.
+x = 4
+y = 4.0
+z = "HackerRank"
+# Read and save an integer, double, and String to your variables.
+i = int(input())
+d = float(input())
+s = str(input())
+
+# Print the sum of both integer variables on a new line.
+print(i + x)
+
+# Print the sum of the double variables on a new line.
+print(d + y)
+# Concatenate and print the String variables on a new line
+print(z + " " + s)
+# The 's' variable above should be printed first.
